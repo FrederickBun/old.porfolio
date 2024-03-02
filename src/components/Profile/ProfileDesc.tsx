d308a23e180b6fdb8dae568343af894331aa4fb9
Fengyi Chen <fengyichen126@qq.com>
2024-03-02 21:05:04 +0800

    Update ProfileDesc.tsx

--- src/components/Profile/ProfileDesc.tsx
+++ src/components/Profile/ProfileDesc.tsx
@@ -99,10 +99,14 @@ export default function ProfileDesc() {
 						的OIer～
 					</p>
 					<p className="text-left text-gray-800 text-xl font-normal pt-2 mt-2">
-						喜欢做前端设计与开发。
+						作为一名信竞选手，C++自然是不多说的～不过我闲时也喜欢做前端设计与开发。
 					</p>
 					<p className="text-left text-gray-800 text-xl font-normal pt-2 mt-2">
-						很高兴在这里与你相遇！！
+						很高兴在这里与你见面！！如果你想联系我，可以发邮箱至 
+						<span className=" bg-clip-text text-transparent bg-gradient-to-r from-orange-500 to-green-500 font-semibold">
+							<a href="mailto:seventeen@ohdragonboi.cn">seventeen@ohdragonboi.cn </a>
+						</span>
+						，期待与你相识～
 					</p>
 				</div>
 			</div>
