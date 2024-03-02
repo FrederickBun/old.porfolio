0bf22ad6171eb3598f1daa2240a1548e7d981b64
Fengyi Chen <fengyichen126@qq.com>
2024-03-02 15:47:58 +0800

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -57,16 +57,22 @@ export default function Home() {
       </motion.section>
 
       {/* Projects Section */}
-			<section
+	  <motion.section
 				ref={section3}
 				id="section3"
-				className="flex flex-col items-center justify-center h-screen"
+				className="flex flex-col sm:flex-row items-center justify-center h-screen w-full sm:w-4/5 p-2 sm:p-20 gap-4 sm:gap-10 "
+        // style={{
+        //   backgroundImage: "linear-gradient(to right, rgb(243,244,246) 1.2px, transparent 1.2px), linear-gradient(to bottom, rgb(243,244,246) 1.2px, transparent 1.2px);",
+        //   backgroundSize: "20px 20px",
+        //   backgroundPosition: "-10px -10px",
+        // }}
+				initial="offscreen"
+				whileInView="onscreen"
+        viewport={{ once: true, amount: 0.2 }}
 			>
-				<div className="font-bold text-8xl">
-					<h1>Section 3</h1>
-				</div>
-			</section>
-		<ProfileDesc />
+        <ProgrammingLang />
+        
+      </motion.section>
       {/* Contact Section */}
       <section
 				ref={section4}
