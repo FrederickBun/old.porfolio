dbb480c0d64ba9928f491603eeb414b65892168b
Fengyi Chen <fengyichen126@qq.com>
2024-03-02 15:54:24 +0800

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -61,16 +61,11 @@ export default function Home() {
 				ref={section3}
 				id="section3"
 				className="flex flex-col sm:flex-row items-center justify-center h-screen w-full sm:w-4/5 p-2 sm:p-20 gap-4 sm:gap-10 "
-        // style={{
-        //   backgroundImage: "linear-gradient(to right, rgb(243,244,246) 1.2px, transparent 1.2px), linear-gradient(to bottom, rgb(243,244,246) 1.2px, transparent 1.2px);",
-        //   backgroundSize: "20px 20px",
-        //   backgroundPosition: "-10px -10px",
-        // }}
 				initial="offscreen"
 				whileInView="onscreen"
         viewport={{ once: true, amount: 0.2 }}
 			>
-        <ProfileDesc />
+        <ProgrammingLang />
         
       </motion.section>
       {/* Contact Section */}
